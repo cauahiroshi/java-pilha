c9df96e301d8dc5b56ef833dd2cc239aa49b6e58
user_acronus <user_acronus@PIN0581930W10-1.senacsp.edu.br>
2022-04-19 16:44:42 -0300

    Terminando parte 4

--- java-pilha/src/FluxoComTratamento.java
+++ java-pilha/src/FluxoComTratamento.java
@@ -1,11 +1,11 @@
 
-public class Fluxo {
+public class FluxoComTratamento {
 
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
 		try {
 			metodo1();
-		} catch (ArithmeticException | NullPointerException | MinhaExcecao  ex) {
+		} catch (ArithmeticException | NullPointerException  ex) {
 			String msg = ex.getMessage();
 			System.out.println("Exception " + msg);
 			ex.printStackTrace();
@@ -13,23 +13,24 @@ public class Fluxo {
 		System.out.println("Fim do main");
 	}
 
-	private static void metodo1() throws MinhaExcecao {
+	private static void metodo1() {
 		System.out.println("Ini do metodo1");
 		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 
-	private static void metodo2() throws MinhaExcecao {
+	private static void metodo2() {
 		System.out.println("Ini do metodo2");
-		
-		throw new MinhaExcecao("deu errado");
-		//ArithmeticException exception = new ArithmeticException("deu errado");
-		//throw exception; 
-		
-		//System.out.println("Fim do metodo2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			int a = i / 0;
+			Conta c = null;
+			c.deposita();
+
+		}
+		System.out.println("Fim do metodo2");
 	}
 }
-
 
 //
 //
